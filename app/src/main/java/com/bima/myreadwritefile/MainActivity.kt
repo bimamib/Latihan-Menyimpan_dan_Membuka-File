b65ce4c1516b49a87816527cbfad66fc9c4e1b43
bimamib <bimaprast1601@gmail.com>
2023-09-20 00:30:00 +0700

    menambahkan metode untuk menyimpan berkas

--- app/src/main/java/com/bima/myreadwritefile/MainActivity.kt
+++ app/src/main/java/com/bima/myreadwritefile/MainActivity.kt
@@ -26,9 +26,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (view.id) {
             R.id.button_new -> newFile()
             R.id.button_open -> showList()
-            R.id.button_save ->
+            R.id.button_save -> saveFile()
         }
     }
+
 
     private fun newFile() {
         binding.editTitle.setText("")
@@ -50,5 +51,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.editTitle.setText(fileModel.filename)
         binding.editFile.setText(fileModel.data)
         Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveFile() {
+        when {
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Kontent harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            else -> {
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
