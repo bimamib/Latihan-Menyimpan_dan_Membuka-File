2f72b1ee5a4a1d90c395c8c2d8d315f0d1f192f0
bimamib <bimaprast1601@gmail.com>
2023-09-20 00:19:41 +0700

    menambahkan code pada MainActivity, dan menambahkan View Binding

--- app/src/main/java/com/bima/myreadwritefile/MainActivity.kt
+++ app/src/main/java/com/bima/myreadwritefile/MainActivity.kt
@@ -2,10 +2,28 @@ package com.bima.myreadwritefile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bima.myreadwritefile.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNew.setOnClickListener(this)
+        binding.buttonOpen.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (view.id) {
+            R.id.button_new ->
+            R.id.button_open ->
+            R.id.button_save ->
+        }
     }
 }
